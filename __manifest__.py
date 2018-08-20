d9034f468eac4bccdf0c01833f0ac3cb9c95ed5d
MitchelHdz <sebastianvicky_@hotmail.com>
2018-08-20 10:27:15 -0500

    [ADD] First commit for frontend odoo course

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Academy BnF",
+
+    'summary': """Manage trainigns:""",
+
+    'description': """
+        Open academy module for managing trainings:
+        - training courses
+        - training sessions
+        - attendees registration
+    """,
+
+    'author': "Ceviche 2.00",
+    'website': "http://www.benandfrank.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
